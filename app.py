112ff60156c042985df3ca75494d54a52609fc66
AlvinRai <22611157@students.uii.ac.id>
2024-07-21 15:01:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ model = joblib.load('best_model.pkl')
 # Memuat data untuk pengkodean dan penskalaan
 data = pd.read_csv('onlinefoods.csv')
 
+# Menambahkan kategori 'Unknown' ke setiap kolom kategorikal selama pelatihan
+for column in data.select_dtypes(include=['object']).columns:
+    data[column] = data[column].astype(str)
+    if 'Unknown' not in data[column].unique():
+        data[column] = data[column].append(pd.Series(['Unknown']))
+
 # Pra-pemrosesan data
 label_encoders = {}
 for column in data.select_dtypes(include=['object']).columns:
     le = LabelEncoder()
-    le.fit(data[column].astype(str))  # Memastikan semua data adalah string
-    data[column] = le.transform(data[column].astype(str))
+    le.fit(data[column])
+    data[column] = le.transform(data[column])
     label_encoders[column] = le
 
 scaler = StandardScaler()
@@ -25,10 +31,13 @@ data[numeric_features] = scaler.fit_transform(data[numeric_features])
 def preprocess_input(user_input):
     processed_input = {}
     for column in label_encoders:
-        input_value = str(user_input[column])
-        if input_value not in label_encoders[column].classes_:
-            input_value = 'Unknown'
-        processed_input[column] = label_encoders[column].transform([input_value])[0]
+        if column in user_input:
+            input_value = str(user_input[column])
+            if input_value not in label_encoders[column].classes_:
+                input_value = 'Unknown'
+            processed_input[column] = label_encoders[column].transform([input_value])[0]
+        else:
+            processed_input[column] = label_encoders[column].transform(['Unknown'])[0]
     processed_input = pd.DataFrame(processed_input, index=[0])
     processed_input[numeric_features] = scaler.transform(processed_input[numeric_features])
     return processed_input
